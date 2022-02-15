418890aa6a91dcc434725a6e02c3cc467495686b
jacopo1996 <jacopo1996@live.it>
2022-02-15 16:05:01 +0100

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,21 @@
+//per nascondere la barra di scorrimento laterale
 document.body.style.overflow = "hidden";
 
+//variabili oggetti documento
 let less = document.querySelector('[counter-less]');
 let more = document.querySelector('[counter-more]');
 let reset = document.querySelector('[counter-reset]');
 let config = document.querySelector('[counter-config]');
 let number = document.querySelector('[counter-value]');
 
+//impostazione iniziale del counter a zero
 let counter = 0;
 number.innerHTML = counter;
 
-
+//click sul pulsante "-"
 less.onclick = function() {
   if (counter == 0) {
-    Swal.fire({
+    Swal.fire({ //messaggio avviso contatore già a zero
       title: "Il contatore è già a zero",
       showCancelButton: false,
       confirmButtonColor: "#22b14c"
@@ -23,20 +26,22 @@ less.onclick = function() {
   }
 }
 
+//click sul pulsante "+"
 more.onclick = function() {
   counter += 1;
   number.innerHTML = counter;
 }
 
+//click sul pulsante "AZZERA"
 reset.onclick = function() {
   if (counter == 0) {
-    Swal.fire({
+    Swal.fire({ //messaggio avviso contatore già a zero
       title: "Il contatore è già a zero",
       showCancelButton: false,
       confirmButtonColor: "#22b14c"
     });
   } else {
-    Swal.fire({
+    Swal.fire({ //messaggio di avviso prima di azzerare
       title: "Vuoi azzerare il contatore?",
       showCancelButton: false,
       showDenyButton: true,
@@ -45,11 +50,11 @@ reset.onclick = function() {
       confirmButtonColor: "#22b14c",
       denyButtonColor: "#ff0000"
     }).then((result) => {
-      if (result.isConfirmed) {
+      if (result.isConfirmed) { //controllo sul pulsante di conferma del messaggio
         counter = 0;
         number.innerHTML = counter;
         if (counter == 0) {
-          Swal.fire({
+          Swal.fire({ //messaggio per confermare che il contatore è stato azzerato
             icon: "success",
             title: "Contatore azzerato con successo",
             showConfirmButton: false,
@@ -61,8 +66,9 @@ reset.onclick = function() {
   }
 }
 
+//click sul pulsante "CONFIGURA"
 config.onclick = function() {
-  Swal.fire({
+  Swal.fire({ //messaggio per impostare numero il numero di partenza
     title: 'Da quale numero deve partire il contatore?',
     input: 'number',
     inputLabel: 'Scrivi il numero',
@@ -71,15 +77,15 @@ config.onclick = function() {
     confirmButtonColor: "#22b14c",
     cancelButtonColor: "#ff0000",
     inputValidator: (value) => {
-      if (!value) {
+      if (!value) { //controllo se il valore inserito è un numero
         return 'Devi inserire un numero!'
-      } else if (Number(value) < 0) {
-        return 'Devi inserire un numero maggiore di zero!'
+      } else if (Number(value) < 0) { // controllo se il numero inserito è maggiore o uguale a zero
+        return 'Devi inserire un numero maggiore o uguale a zero!'
       } else {
         counter = Number(value);
         number.innerHTML = counter;
         if (counter == Number(value)) {
-          Swal.fire({
+          Swal.fire({ //messaggio per confermare che il contatore è stato correttamente impostato
             icon: "success",
             title: `Contatore impostato a ${ counter } con successo`,
             showConfirmButton: false,
